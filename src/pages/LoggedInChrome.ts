2f6e14666a605c1fff67a10e18950a35f514956a
Stan M <iamstanstanstan@gmail.com>
2026-09-04 23:23:08 -0500

    make a better locator for logout

--- src/pages/LoggedInChrome.ts
+++ src/pages/LoggedInChrome.ts
@@ -3,7 +3,6 @@ import type { Locator, Page } from '@playwright/test';
 export class LoggedInChrome {
   readonly page: Page;
   readonly accountName: Locator;
-  readonly userMenu: Locator;
   readonly logOut: Locator;
 
   constructor(page: Page, accountName: string) {
@@ -12,12 +11,13 @@ export class LoggedInChrome {
       .getByRole('banner')
       .getByRole('button', { name: accountName, exact: true })
       .or(page.getByRole('button', { name: accountName, exact: true }));
-    this.userMenu = page.locator('#user_menu__BV_toggle_');
-    this.logOut = page.locator('#logout_button');
+    this.logOut = page
+      .getByRole('menuitem', { name: /log out/i })
+      .or(page.getByRole('button', { name: /log out/i }));
   }
 
   async logout(): Promise<void> {
-    await this.userMenu.evaluate((el: HTMLElement) => el.click());
-    await this.logOut.evaluate((el: HTMLElement) => el.click());
+    await this.accountName.click();
+    await this.logOut.click();
   }
 }
